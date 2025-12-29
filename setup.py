0ec9cba02383c8f7b15fbc29f0f04438357f0dab
croissantfish <croissantfish@outlook.com>
2025-12-29 22:59:04 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='m3dns',
-    version='1.0.0',
+    version='1.0.2',
     description='Multiple Devices Dynamic DNS(`m3dns`) is a python package to updating nameservers for multiple devices'
                 ' in a subnet, especially for a IPv6 subnet with a prefix shorter than 64.',
-    author='BreakVoid',
-    author_email='fishtara@outlook.com',
-    url='https://github.com/BreakVoid/m3dns',
+    author='croissantfish',
+    author_email='croissantfish@outlook.com',
+    url='https://github.com/croissantfish/m3dns',
     packages=['m3dns'],
     requires=[
         'requests',
